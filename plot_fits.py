913dc1b6a7296aaa27bde8bf671bd4c6a9441635
Drew Linsley <drewlinsley@gmail.com>
2017-11-06 15:37:48 -0500

    Fixed colorbars in ALLEN plotting.

--- plot_fits.py
+++ plot_fits.py
@@ -32,7 +32,8 @@ def plot_fits(
             # y: files['dataset_method'].item()['y_min'],
         }
         exp_name = {
-            'experiment_name': data['dataset_method'].item()['experiment_name']}
+            'experiment_name': data['dataset_method'].item()[
+                'experiment_name']}
         perf, mt = query_hp_hist(exp_name, pass_creds=db_config)
         if perf is None:
             print 'No fits for: %s' % exp_name['experiment_name']
@@ -47,14 +48,20 @@ def plot_fits(
             model_types += [mt]
 
     # Package as a df
-    lcs = ['red', '#05D9F6', '#E1FA7']
-    hcs = ['white', '#5011D1', '#F46FEE'] 
     xs = np.round(np.asarray(xs) * 10).astype(int)
     ys = np.round(np.asarray(ys) * 10).astype(int)
     perfs = np.asarray(perfs)
     model_types = np.asarray(model_types)
     umt, model_types_inds = np.unique(model_types, return_inverse=True)
-    cps = [sns.cubehelix_palette(n_colors=100, as_cmap=True), sns.cubehelix_palette(n_colors=100, as_cmap=True, rot=-.4), sns.cubehelix_palette(n_colors=100, as_cmap=True, rot=.1, start=2.8)]
+    cps = [
+        'Reds',
+        'Blues',
+        'Reds'
+        # sns.cubehelix_palette(n_colors=100, as_cmap=True, start=10.),
+        # sns.cubehelix_palette(n_colors=100, as_cmap=True, rot=-.4),
+        # sns.cubehelix_palette(n_colors=100, as_cmap=True, rot=.1, start=2.8)
+        ]
+
     # cps = ["Blues", "Reds", "Greens"]
     f, ax = plt.subplots()
     p = []
@@ -62,57 +69,22 @@ def plot_fits(
         it_xs = xs[model_types == imt] + (idx * 1)
         it_ys = ys[model_types == imt] + (idx * 1)
         it_perfs = perfs[model_types == imt]
-        df = pd.DataFrame(np.vstack((it_xs, it_ys, it_perfs)).transpose(), columns=['x', 'y', 'perf'])
+        # df = pd.DataFrame(np.vstack((it_xs, it_ys, it_perfs)).transpose(), columns=['x', 'y', 'perf'])
         # p += [sns.lmplot(x='x', y='y', hue='perf', palette=cp, data=df, markers='s', fit_reg=False, x_jitter=1, y_jitter=1)]
-        p += [ax.scatter(it_xs, it_ys, c=it_perfs, cmap=cp)]
-        print (imt)
-    [plt.colorbar(ip, ticks=np.linspace(-.1, 1, 100, endpoint=True)) for ip in p]
-    ax.legend()
+        p += [ax.scatter(it_xs, it_ys, c=it_perfs, cmap=cp, label=imt)]
+    plt.legend()
+    legend = ax.get_legend()
+    legend.legendHandles[0].set_color(plt.cm.Reds(.8))
+    legend.legendHandles[1].set_color(plt.cm.Blues(.8))
+    legend.legendHandles[2].set_color(plt.cm.Reds(.8))
+    [plt.colorbar(
+        ip, ticks=np.linspace(
+            -.1, 1, 100, endpoint=True)) for ip in p]
     plt.xlim([-100, 1500 * 10])
     plt.ylim([-100, 1000 * 10])
     # plt.xlabel('X-axis degrees of visual-angle')
     # plt.xlabel('Y-axis degrees of visual-angle')
     plt.show()
-    # for idx, imt in enumerate(umt):
-    #     it_xs = xs[mt == imt]
-    #     it_ys = ys[mt == imt]
-    #     it_perfs = perfs[mt == imt]
-    #     df = pd.DataFrame(np.vstack((it_xs, it_ys, it_perfs)).transpose(), columns=['x', 'y', 'perf'])
-    #     if idx == 0:
-    #         p = ggplot(df, aes(x='x', y='y', color='perf'))
-    #     else:
-    #         p += ggplot(df, aes(x='x', y='y', color='perf'))
-    #     p += geom_jitter()
-    #     p += scale_color_gradient(low=lcs[idx], high=hcs[idx])
-    # p += xlab('X-axis degrees of visual-angle')
-    # p += ylab('Y-axis degrees of visual-angle')
-    # p += xlim(low=0, high=1500)
-    # p += ylim(low=0, high=1000)
-    # p += ggtitle('V1 fits')
-    # p.save('test.png')
-
-    # df = pd.DataFrame(np.vstack((xs, ys, perfs)).transpose(), columns=['x', 'y', 'perf'])
-    # p = ggplot(df, aes(x='x', y='y', color='perf')) +\
-    # geom_point() +\
-    # scale_color_gradient(low='#05D9F6', high='#5011D1') +\
-    # xlab('X-axis degrees of visual-angle') +\
-    # ylab('Y-axis degrees of visual-angle') +\
-    # xlim(low=0, high=1500) +\
-    # ylim(low=0, high=1000) +\
-    # ggtitle('V1 fits')
-    # p.save('test.png')
-    # Plot results
-
-    # f, ax = plt.subplots()
-    # cmap = plt.get_cmap('magma')
-    # ls = []
-    # for idx, d in enumerate(out_data):
-    #     x = np.round(d['x'] * 10)
-    #     y = np.round(d['y'] * 10)
-    #     l = plt.scatter(x, y, marker='s', c=cmap(idx*100))
-    #     ls += [l]
-    # plt.colorbar(ls)
-    # plt.show()
 
 
 if __name__ == '__main__':
@@ -125,4 +97,3 @@ if __name__ == '__main__':
         help='Name of experiment.')
     args = parser.parse_args()
     plot_fits(**vars(args))
-
